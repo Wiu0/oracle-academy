3fe3f035d07c5df269e7fc21566221f19909da84
wiu <williamnoronha@hotmail.com>
2022-09-03 11:00:33 -0300

    IMC: if, else, else if

--- gf2022/primeiro-passo/src/IMC.java
+++ gf2022/primeiro-passo/src/IMC.java
@@ -9,14 +9,30 @@ public class IMC {
 		imc = massa/(altura * altura);
 		System.out.println("O seu IMC é " + imc + ".");
 		
+		if(imc < 18.5) {
+			System.out.println("Seu indice é: Magreza");
+		}
 		
+		if(imc >= 18.5 && imc < 24.9) {
+			System.out.println("Seu indice é: Normal");
+		}
+		
+		if(imc >= 24.9 && imc < 30) {
+			System.out.println("Seu indice é: Sobrepeso");
+		}
+		
+		if(imc > 30) {
+			System.out.println("Seu indice é: Obesidade");
+		}
 		//SE, if
-		
 		
 		//se imc < 18.5  				:Magreza
 		//se imc >= 18.5 imc < 24.9		:Normal
 		//se imc >= 24.9 imc < 30		:Sobrepeso
 		//se imc >= 30					:Obesidade
+		
+		
+		
 		
 		
 	}
---
+++ gf2022/primeiro-passo/src/IMC2.java
@@ -0,0 +1,32 @@
+
+public class IMC2 {
+
+	public static void main(String[] args) {
+		double massa = 176;
+		double altura = 1.86;
+
+		double imc;
+		imc = massa / (altura * altura);
+		System.out.println("O seu IMC é " + imc + ".");
+
+		if (imc < 18.5) {
+			System.out.println("Seu indice é: Magreza");
+		} else {
+
+			if (imc >= 18.5 && imc < 24.9) {
+				System.out.println("Seu indice é: Normal");
+			} else {
+				if (imc >= 24.9 && imc < 30) {
+					System.out.println("Seu indice é: Sobrepeso");
+				} else {
+//					if (imc > 30) {
+						System.out.println("Seu indice é: Obesidade");
+//					}
+				}
+
+			}
+
+		}
+
+	}
+}
