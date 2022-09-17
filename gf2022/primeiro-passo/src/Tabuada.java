11cfc198724a965f987f7120c5499f77441b24cd
wiu <williamnoronha@hotmail.com>
2022-09-17 12:12:55 -0300

    solução tabuada

---
+++ gf2022/primeiro-passo/src/Tabuada.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite um número a ser multiplicado");
+		int numero = scan.nextInt();
+		
+		System.out.println("Quantas vezes o número será multiplicado?");
+		int limiteLaco = scan.nextInt();
+		
+		scan.close();
+		
+		for (int i = 0; i <= limiteLaco; i++) {
+			System.out.print(numero + " x " + i + " = ");
+			System.out.println(numero * i);
+
+		}
+	}
+}
