100bbc830b7ba642f81d89229ac3511e9698fe19
wiu <williamnoronha@hotmail.com>
2022-09-17 09:59:15 -0300

    print e println, escape

---
+++ gf2022/primeiro-passo/src/ApresentandoValor.java
@@ -0,0 +1,23 @@
+
+public class ApresentandoValor {
+
+	public static void main(String[] args) {
+		System.out.println(12);
+		System.out.println("Wiu");
+		System.out.print(12);
+		System.out.print("Wiu");
+		System.out.println("Norte");
+		
+		//System.err.println("Oi");
+		
+		System.out.println("João é o \"cara\"");
+		System.out.println("Nunca usar o simbolo \\");
+		System.out.println("Linha superior \nLinha inferior");
+		System.out.print("\tTudo");
+		
+		
+	}
+	//Joao é o "cara"
+	//Nunca usar o simbomo \
+	
+}
